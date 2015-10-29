cd71965a3a7fea3de2f4b4d8deb54a1214f6e436
unknown <stefan.cerny@gmail.com>
2015-10-29 10:07:32 +0100

    XXXX

--- update.php
+++ update.php
@@ -6,6 +6,8 @@ $output = shell_exec('git config --global user.email stefan.cerny@gmail.com');
      //$output = shell_exec('git commit -m "'.$_POST["txt"].'" ');
     $output = shell_exec('git commit -m "XXXX" ');
     echo "Commit:<pre>$output</pre>";
+    $output = shell_exec('git push ');
+    echo "Commit:<pre>$output</pre>";
 if($_GET['action']=='add'){
 
 }
